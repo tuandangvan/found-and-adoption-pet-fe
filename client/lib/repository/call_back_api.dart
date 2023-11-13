ab3fb73c9cc66b4fef8e9cd6cf4577b7ec34399d
username <tuandangvann@gmail.com>
2023-11-14 03:37:22 +0700

    call api profile center

--- client/lib/repository/call_back_api.dart
+++ client/lib/repository/call_back_api.dart
@@ -1,15 +1,10 @@
 import 'dart:convert';
 
-import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:found_adoption_application/repository/auth_api.dart';
 
 Future<dynamic> callBackApi(apiUrl, method, body) async {
-  var userBox = await Hive.openBox('userBox');
-  var currentUser = userBox.get('currentUser');
   var newAccessToken = refreshAccessToken().toString();
-  currentUser.accessToken = newAccessToken;
-  userBox.put('currentUser', currentUser);
   var response;
   if (method == "post") {
     response = await http.post(apiUrl,
--- client/lib/repository/profile_api.dart
+++ client/lib/repository/profile_api.dart
@@ -1,24 +1,20 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:found_adoption_application/models/userCenter.dart';
 import 'package:found_adoption_application/models/userInfo.dart';
 import 'package:found_adoption_application/repository/call_back_api.dart';
-// import 'package:found_adoption_application/models/current_user.dart';
-// import 'package:found_adoption_application/screens/user_screens/menu_frame.dart';
-import 'package:hive/hive.dart';
-import 'package:found_adoption_application/repository/auth_api.dart';
-
+import 'package:found_adoption_application/utils/getCurrentClient.dart';
 import 'package:http/http.dart' as http;
 
 Future<InfoUser> getProfile(BuildContext context) async {
-  var userBox = await Hive.openBox('userBox');
-  var currentUser = userBox.get('currentUser');
-  var userId = currentUser.id;
-  var accessToken = currentUser.accessToken;
+  var currentClient = await getCurrentClient();
+  var id = currentClient.id;
+  var accessToken = currentClient.accessToken;
   var user;
   try {
     var responseData;
-    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/user/${userId}");
+    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/user/${id}");
 
     var response = await http.get(apiUrl, headers: {
       'Authorization': 'Bearer ${accessToken}',
@@ -27,17 +23,7 @@ Future<InfoUser> getProfile(BuildContext context) async {
     // print('Response get ALL POST: $responseData');
 
     if (responseData['message'] == 'jwt expired') {
-      //Làm mới accessToken bằng Future<String> refreshAccessToken(), gòi tiếp tục gửi lại request cũ
-      var newAccessToken = refreshAccessToken().toString();
-      print('Token mới: $newAccessToken');
-      currentUser.accessToken = newAccessToken;
-      userBox.put('currentUser', currentUser);
-
-      response = await http.post(apiUrl, headers: {
-        'Authorization': 'Bearer $newAccessToken',
-      });
-
-      responseData = json.decode(response.body);
+      responseData = callBackApi(apiUrl, "get", "");
     }
     var userData = responseData['user'] as dynamic;
     user = InfoUser.fromJson(userData);
@@ -48,20 +34,26 @@ Future<InfoUser> getProfile(BuildContext context) async {
 }
 
 Future<void> updateProfile(
-    BuildContext context, firstName, lastName, phoneNumber, address) async {
-  var userBox = await Hive.openBox('userBox');
-  var currentUser = userBox.get('currentUser');
-  var userId = currentUser.id;
-  var accessToken = currentUser.accessToken;
+    BuildContext context, firstName, lastName, phoneNumber, address, experience) async {
+  var currentClient = await getCurrentClient();
+  var id = currentClient.id;
+  var accessToken = currentClient.accessToken;
   try {
     var responseData;
-    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/user/${userId}");
-    var body = jsonEncode(<String, String>{
+    var body = jsonEncode(<String, dynamic>{
       if (firstName != "") 'firstName': firstName,
       if (lastName != "") 'lastName': lastName,
       if (phoneNumber != "") 'phoneNumber': phoneNumber,
-      if (address != "") 'address': address
+      if (address != "") 'address': address,
+      'experience': experience
     });
+
+    if (body == jsonEncode(<String, String>{})) {
+      print("No something change!");
+      return;
+    }
+
+    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/user/${id}");
 
     var response = await http.put(apiUrl,
         headers: {
@@ -73,6 +65,73 @@ Future<void> updateProfile(
 
     if (responseData['message'] == 'jwt expired') {
       responseData = await callBackApi(apiUrl, "put", body);
+    }
+    if (responseData['success']) {
+      print("update success");
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
+Future<InfoCenter> getProfileCenter(BuildContext context) async {
+  var currentClient = await getCurrentClient();
+  var id = currentClient.id;
+  var accessToken = currentClient.accessToken;
+  var center;
+  try {
+    var responseData;
+    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/center/${id}");
+
+    var response = await http.get(apiUrl, headers: {
+      'Authorization': 'Bearer ${accessToken}',
+    });
+    responseData = json.decode(response.body);
+
+    if (responseData['message'] == 'jwt expired') {
+      responseData = callBackApi(apiUrl, "get", "");
+    }
+    var centerData = responseData['center'] as dynamic;
+    center = InfoCenter.fromJson(centerData);
+  } catch (e) {
+    print(e);
+  }
+  return center;
+}
+
+Future<void> updateProfileCenter(
+    BuildContext context, name, phoneNumber, address) async {
+  var currentClient = await getCurrentClient();
+  var id = currentClient.id;
+  var accessToken = currentClient.accessToken;
+  try {
+    var responseData;
+    var body = jsonEncode(<String, String>{
+      if (name != "") 'name': name,
+      if (phoneNumber != "") 'phoneNumber': phoneNumber,
+      if (address != "") 'address': address
+    });
+
+    if (body == jsonEncode(<String, String>{})) {
+      print("No something change!");
+      return;
+    }
+
+    final apiUrl = Uri.parse("http://10.0.2.2:8050/api/v1/center/${id}");
+
+    var response = await http.put(apiUrl,
+        headers: {
+          'Authorization': 'Bearer ${accessToken}',
+          'Content-Type': 'application/json',
+        },
+        body: body);
+    responseData = json.decode(response.body);
+
+    if (responseData['message'] == 'jwt expired') {
+      responseData = await callBackApi(apiUrl, "put", body);
+    }
+    if (responseData['success']) {
+      print("update success");
     }
   } catch (e) {
     print(e);
