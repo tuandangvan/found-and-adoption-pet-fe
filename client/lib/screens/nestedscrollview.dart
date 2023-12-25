ecae75a41fcee6b329cc456240cb1611ba9021c2
DoMyLan <mylan060401@gmail.com>
2023-12-25 16:35:26 +0700

    Wrap content

---
+++ client/lib/screens/nestedscrollview.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyNestedScrollView(),
+    );
+  }
+}
+
+class MyNestedScrollView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                
+                background: Image.asset(
+                  'assets/images/Lan.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: ListView.builder(
+          itemCount: 50,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('Item $index'),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
